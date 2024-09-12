bcef513099b805c2c956e2dc106dfc97d977fe07
Matias Candeloro <matias_candeloro@hotmail.com>
2024-09-12 14:47:22 -0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { StudentComponent } from './components/student/student.component';
+import { FootBarComponent } from './components/foot-bar/foot-bar.component';
+import { CourseComponent } from './components/course/course.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    SideBarComponent,
+    StudentComponent,
+    FootBarComponent,
+    CourseComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
